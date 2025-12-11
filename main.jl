0f7fc2ef6c9c4722764ef43dd2952b00e6d3eccf
kyystt <gui.sobreira10@gmail.com>
2025-12-11 00:54:41 -0300

    trabalho terminado

--- main.jl
+++ main.jl
@@ -1,3 +1,4 @@
+using PrettyTables
 using HTTP
 using JSON
 using CSV
@@ -14,11 +15,13 @@ rename!(df, "Loud (Db)" => :Loudness, "BPM" => :Tempo)
 
 # Limpa os dados
 select!(df, :Song, :Dance, :Energy, :Acoustic, :Instrumental, :Happy, :Loudness, :Tempo, :Speech, :Live)
+pretty_table(df)
 boss_names = df.Song
 select!(df, Not(:Song))
 
 # Pre processamento
 features = Matrix{Float64}(df)'
+
 
 function normalizaDados(M)
     M_norm = copy(M)
@@ -167,5 +170,98 @@ anim = @animate for i in 0:2:360
 end
 
 nome_gif = "animacao_souls.gif"
-gif(anim, nome_gif, fps = 15)
+#gif(anim, nome_gif, fps = 15)
 println("[*] Mapa 3D salvo em $nome_gif")
+
+println("[*] Comecando plot em 3D dos dados")
+p = scatter3d(xs, ys, zs,
+    title = "Universo Souls",
+    xlabel = "PC1 (Complexidade)",
+    ylabel = "PC2 (Atmosfera)",
+    zlabel = "PC3 (Energia)",
+    legend = false,
+    color = :darkred,
+    markersize = 5,
+    alpha = 0.8,
+    size = (900, 700),
+    camera = (45, 30)# <--- AQUI A MÁGICA ACONTECE
+)
+
+# Plota os textos
+plot!(p, annotations = [
+    (xs[j], ys[j], zs[j], text(boss_names[j], 8, :black, :bottom)) 
+    for j in ids_anotar
+])
+
+nome_plot3d = "plot3d_souls.png"
+savefig(p, nome_plot3d)
+println("[*] Plot salvo em $nome_plot3d")
+
+
+function recomendaChefe(inputUser::Vector{Float64}, matrizCompleta, nomes)
+    ranking = []
+
+    num_chefes = size(matrizCompleta, 2)
+
+    for i in 1:num_chefes
+        features_chefe = matrizCompleta[:, i]
+
+        dist = norm(inputUser - features_chefe)
+
+        push!(ranking, (nomes[i], dist))
+    end
+
+    sort!(ranking, by = x -> x[2])
+
+    return ranking
+end
+
+function coletaDados()
+    println("[*] Coletando dados do usuario...")
+    println("Responda de 1.0 (minimo) a 5.0 (maximo)")
+
+    function perguntar(txt)
+        print("$txt [1.0 - 5.0]: ")
+        input = readline()
+
+        input = replace(input, "," => ".")
+
+        valor = tryparse(Float64, input)
+        if isnothing(valor) || valor < 1.0 || valor > 5.0
+            println("Valor invalido, assumindo neutro (3.0)")
+            return 0.0
+        end
+
+        return (valor - 3.0)
+    end
+
+    p_intensa = perguntar("1. Quão intensa/barulhenta deve ser a música?")
+    p_rapida = perguntar("2. Quão rápida deve ser a música?")
+    p_feliz = perguntar("3. A música deve ser triste (1) ou heróica/vitoriosa (5)?")
+    p_acustica = perguntar("4. Prefere orquestra pesada (1) ou instrumento solo/acústico (5)?")
+    p_vozes = perguntar("5. Prefere apenas instrumentos (1) ou coral/vozes (5)?")
+    p_ritmo = perguntar("6. A musica deve ser caotica/quebrada (1) ou constante/dancante (5)?")
+
+    vetor_final = zeros(9)
+
+    vetor_final[1] = p_ritmo * 0.8
+    vetor_final[2] = p_intensa
+    vetor_final[3] = p_acustica
+    vetor_final[4] = -1.0 * p_vozes
+    vetor_final[5] = p_feliz
+    vetor_final[6] = p_intensa
+    vetor_final[7] = p_rapida
+    if p_vozes > 1.0
+        vetor_final[8] = 0.5
+    else
+        vetor_final[8] = -1.0
+    end
+    vetor_final[9] = 0.0
+
+    return vetor_final
+end
+
+perfil_user = coletaDados()
+recs = recomendaChefe(perfil_user, data_matrix, boss_names)
+
+println("Você deveria ouvir: $(recs[1][1])")
